97e549a7ef6cf841fa024a5bfbc0912e285b5c47
Ednilton Martins <ednilton17@gmail.com>
2019-05-14 01:04:01 -0300

    Adicionando mongoose e vários modelos

--- config/environment/variables.js
+++ config/environment/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database:{
+        connection: process.env.connection || 'mongodb+srv://nofood_adm:PaYcy5dguKn7CrYW@nofood-sjwdz.mongodb.net/test?retryWrites=true'
     }
 }
 
