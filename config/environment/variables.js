6c002d7108181f6a9d46f8a36b19278a3a1d7311
ednilton.silva-at-514001089657 <edniton17@gmail.com>
2019-05-15 02:10:03 -0300

    Inserindo os repositories

--- config/environment/variables.js
+++ config/environment/variables.js
@@ -3,7 +3,7 @@ const variables = {
         port: process.env.port || 3000
     },
     Database:{
-        connection: process.env.connection || 'mongodb+srv://nofood_adm:PaYcy5dguKn7CrYW@nofood-sjwdz.mongodb.net/test?retryWrites=true'
+        connection: process.env.connection || 'mongodb+srv://nofood_adm:lvg28QfeqBZ7wFsq@nofood-sjwdz.mongodb.net/nofood?retryWrites=true'
     }
 }
 
--- controllers/categoria-controller.js
+++ controllers/categoria-controller.js
@@ -2,35 +2,35 @@
 require('../models/categoria-model');
 const mongoose = require('mongoose');
 const categoria = mongoose.model('Categoria');
+const repository = require('../repositories/categoria-repository');
+
 
 function categoriaController(){
 
 }
 
 categoriaController.prototype.post = async (req, res) =>{ 
-    let modelo = new categoria(req.body);
-    let resultado = await modelo.save();
+    let resultado = await new repository().create(req.body);
     res.status(201).send(resultado);
 };
 
 categoriaController.prototype.put = async (req, res) =>{ 
-    await categoria.findByIdAndUpdate(req.params.id, {$set : req.body});
-    let categorariaEncontrada = await categoria.findById(req.params.id);
-    res.status(202).send(categorariaEncontrada);
+    let resultado = await new repository().update(req.params.id, req.body);
+    res.status(202).send(resultado);
 };
 
 categoriaController.prototype.get = async (req, res) =>{ 
-    let lista = await categoria.find();
+    let lista = await new repository().getall();
     res.status(200).send(lista);
 };
 
 categoriaController.prototype.getById = async (req, res) =>{ 
-    let categoriaEncontrada =  await categoria.findById(req.params.id);
+    let categoriaEncontrada =  await new repository().getById(req.params.id);
     res.status(200).send(categoriaEncontrada);
 };
 
 categoriaController.prototype.delete = async (req, res) =>{ 
-    let deletado = await categoria.findByIdAndDelete(req.params.id);
+    let deletado = await new repository().delete(req.params.id);
     res.status(204).send(deletado);
 };
 
